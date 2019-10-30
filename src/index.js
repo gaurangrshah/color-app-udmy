e220dca74f8f7a6e87c6c00118b78599adff97a4
gshah2020 <gaurang.r.shah@gmail.com>
2019-10-30 15:33:16 -0400

    adds basic Routing, hardcoded

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
 import { generatePalette } from "./colorHelpers";
 
 function App() {
   return (
-    <div className="App">
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>Palette List Here</h1>} />
+      <Route
+        exact
+        path="/palette/:id"
+        render={() => <Palette palette={generatePalette(seedColors[4])} />}
+      />
+    </Switch>
+    // <div className="App">
+    //   <Palette palette={generatePalette(seedColors[4])} />
+    // </div>
   );
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  rootElement
+);

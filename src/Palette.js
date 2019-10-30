47ad87376c0170abfd448f39b469517753aeb710
gshah2020 <gaurang.r.shah@gmail.com>
2019-10-30 11:09:38 -0400

    adds slider from rc-sliider

--- src/Palette.js
+++ src/Palette.js
@@ -1,24 +1,39 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
-import seedColors from "./seedColors";
-import { generatePalette } from "./colorHelpers";
+import Slider from "rc-slider";
+//github.com/react-component/slider
 import "./Palette.css";
+import "rc-slider/assets/index.css";
 
 class Palette extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = { level: 500 };
+    this.changeLevel = this.changeLevel.bind(this);
+  }
+
+  changeLevel(level) {
+    console.log(level);
+    this.setState({ level });
   }
   render() {
-    console.log(generatePalette(seedColors[4]));
-    // console.log(this.props);
-    const colorboxes = this.props.colors.map(color => (
-      <ColorBox background={color.color} name={color.name} />
+    const { colors } = this.props.palette;
+    const { level } = this.state;
+    // console.log(generatePalette(seedColors[4]));
+    // console.log(colors);
+    const colorboxes = colors[level].map(color => (
+      <ColorBox background={color.hex} name={color.name} />
     ));
-    console.log(this.props);
     return (
       <div className="Palette">
+        <Slider
+          defaultValue={level} // defines starting value for slider
+          min={100} // defines min range
+          max={900} // defines max range
+          step={100} // defines increment
+          onAfterChange={this.changeLevel} // onchange handler
+        />
         {/* Navbar will go here */}
         <div className="Palette-colors">{colorboxes}</div>
         {/* footer will go here */}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
   return (
     <div className="App">
-      <Palette colors={seedColors[2].colors} />
+      <Palette palette={generatePalette(seedColors[4])} />
       <h2>Stage 1</h2>
     </div>
   );

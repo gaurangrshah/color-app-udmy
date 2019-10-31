acb5088dc98071775e5db58d8d2636c9d72c83ea
gshah2020 <gaurang.r.shah@gmail.com>
2019-10-31 02:18:48 -0400

    adds react-chome-picker

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -2,15 +2,20 @@ import React, { Component } from "react";
 import classNames from "classnames";
 import { withStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
+// https://v3.material-ui.com/demos/drawers/#drawer
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import { ChromePicker } from "react-color";
+// https://casesandberg.github.io/react-color/
 
-const drawerWidth = 240;
+const drawerWidth = 400;
 
 const styles = theme => ({
   root: {
@@ -71,7 +76,7 @@ const styles = theme => ({
 
 class NewPaletteForm extends Component {
   state = {
-    open: false
+    open: true
   };
 
   handleDrawerOpen = () => {
@@ -121,6 +126,22 @@ class NewPaletteForm extends Component {
             </IconButton>
           </div>
           <Divider />
+          <Typography variant="h4">Design Your Palette</Typography>
+          <div>
+            <Button variant="contained" color="secondary">
+              CLEAR PALETTE
+            </Button>
+            <Button variant="contained" color="primary">
+              RANDOM COLOR
+            </Button>
+          </div>
+          <ChromePicker
+            color="purple"
+            onChange={newColor => console.log(newColor)}
+          />
+          <Button variant="contained" color="primary">
+            ADD COLOR
+          </Button>
         </Drawer>
         <main
           className={classNames(classes.content, {

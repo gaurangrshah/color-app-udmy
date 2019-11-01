c9e9407b0448676c0987b3f36303004ef4369890
gshah2020 <gaurang.r.shah@gmail.com>
2019-11-01 01:12:35 -0400

    extracts styles from palette form components generates constants.js

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -2,40 +2,8 @@ import React from "react";
 import DeleteIcon from "@material-ui/icons/Delete";
 import { withStyles } from "@material-ui/styles";
 import { SortableElement } from "react-sortable-hoc";
+import styles from "./styles/DraggableColorBoxStyles.js";
 
-const styles = {
-  root: {
-    fontFamily: "Roboto",
-    width: "20%",
-    height: "25%",
-    margin: "0 auto",
-    display: "inline-block",
-    position: "relative",
-    cursor: "pointer",
-    marginBottom: "-3.5px",
-    "&:hover svg": {
-      color: "white",
-      transform: "scale(1.5)"
-    }
-  },
-  boxContent: {
-    position: "absolute",
-    width: "100%",
-    left: "0",
-    bottom: "0",
-    padding: "10px",
-    color: "rgba(0, 0, 0, 0.6)",
-    letterSpacing: "1px",
-    textTransform: "uppercase",
-    fontSize: "12px",
-    zIndex: "0",
-    display: "flex",
-    justifyContent: "space-between"
-  },
-  deleteIcon: {
-    transition: "all 0.3s ease-in-out"
-  }
-};
 const DraggableColorBox = SortableElement(props => {
   const { classes, handleClick, name, color } = props;
   return (
--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -13,68 +13,7 @@ import arrayMove from "array-move";
 import PaletteFormNav from "./PaletteFormNav";
 import DraggableColorList from "./DraggableColorList";
 import ColorPickerForm from "./ColorPickerForm";
-
-const drawerWidth = 400;
-
-const styles = theme => ({
-  // refactors appBar related styles to <PaletteFormNav/>
-  root: {
-    display: "flex"
-  },
-  hide: {
-    display: "none"
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0
-  },
-  drawerPaper: {
-    width: drawerWidth,
-    display: "flex",
-    alignItems: "center"
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: "0 8px",
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end"
-  },
-  newBoxes: {
-    // screen height - appBar height
-    height: "calc(100vh - 64px)"
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing.unit * 3,
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    marginLeft: -drawerWidth
-  },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: 0
-  },
-  container: {
-    width: "90%",
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  buttons: {
-    width: "100%"
-  },
-  button: {
-    width: "50%"
-  }
-});
+import styles from "./styles/NewPaletteFormStyles";
 
 class NewPaletteForm extends Component {
   static defaultProps = {

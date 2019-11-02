24f0016a5c3fb7b475dc74c5b04579aad0f4e48d
gshah2020 <gaurang.r.shah@gmail.com>
2019-11-01 20:47:05 -0400

    adds classNames to ColorBox && fix NavBar Slider responsive size

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import CopyToClipboard from "react-copy-to-clipboard";
 import { withStyles } from "@material-ui/styles";
+import classNames from "classnames";
 import styles from "./styles/ColorBoxStyles";
 
 class ColorBox extends Component {
@@ -30,11 +31,19 @@ class ColorBox extends Component {
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background: background }} className={classes.ColorBox}>
           <div
-            className={`${classes.copyOverlay} ${copied &&
-              classes.showOverlay} `}
             style={{ background }}
+            // className={`${classes.copyOverlay} ${copied &&
+            //   classes.showOverlay} `}
+            className={classNames(classes.copyOverlay, {
+              [classes.showOverlay]: copied
+            })}
           />
-          <div className={`${classes.copyMsg} ${copied && classes.showMsg} `}>
+          <div
+            // className={`${classes.copyMsg} ${copied && classes.showMsg} `}
+            className={classNames(classes.copyMsg, {
+              [classes.showMsg]: copied
+            })}
+          >
             <h1>COPIED!</h1>
             <p className={classes.copyText}>{background}</p>
           </div>
